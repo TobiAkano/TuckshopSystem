e3740c45c2956f5f64e9375f522ad1df156a3f8e
TobiAkano <praisekolawoleakano@gmail.com>
2024-06-10 11:00:43 +0100

    latest files

---
+++ database/migrations/2024_06_09_140253_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->float('price');
+            $table->integer('no_of_items_in_stock');
+            // $table->string('created_by');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
